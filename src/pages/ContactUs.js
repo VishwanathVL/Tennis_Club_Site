0621226b6c290db3a45d99963a0678608fd9c9f6
vishwa <mechvishwanath@gmail.com>
2023-07-26 20:24:40 +0530

    Tennis_Club_Site

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,27 +1,33 @@
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import { Box } from '@mui/material';
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { Box } from "@mui/material";
 
 function ContactUs() {
-    return (
-      <>
+  return (
+    <>
       <Box className="contactus">
-      <h2>Contact US</h2>
-      <a target="_blank" href="https://www.facebook.com/RMSCMudhol/"
-        className="facebook social">
-        <FacebookIcon color="secondary" size="2x" />
-      </a>
-      
-      <a target="_blank" href="https://www.instagram.com/raja_malojirao_sports_club"
-        className="instagram social">
-        <InstagramIcon color="secondary" size="2x" />
-      </a>
-      <p>For more queries contact: <b>malojiraosportsclub@gmail.com</b></p>
-        </Box>
+        <h2>Contact US</h2>
+        <a
+          target="_blank"
+          href="https://www.facebook.com/RMSCMudhol/"
+          className="facebook social"
+        >
+          <FacebookIcon color="secondary" size="2x" />
+        </a>
 
-       
-      </>
-    );
-  }
-  
-  export default ContactUs;
+        <a
+          target="_blank"
+          href="https://www.instagram.com/raja_malojirao_sports_club"
+          className="instagram social"
+        >
+          <InstagramIcon color="secondary" size="2x" />
+        </a>
+        <p>
+          For more queries contact: <b>malojiraosportsclub@gmail.com</b>
+        </p>
+      </Box>
+    </>
+  );
+}
+
+export default ContactUs;
